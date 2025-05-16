9ac8646ab4c3f7a2199f341bafe424a1e841c581
Isaac Talb <thaungahlaybaw@gmail.com>
2025-05-16 16:54:53 +0700

    update for shareable content

--- resources/views/frontend/post/index.blade.php
+++ resources/views/frontend/post/index.blade.php
@@ -15,9 +15,9 @@
                         <h1>{{ $post->title }}</h1>
                         <ul class="entry-meta">
                             <li class="post-author-img"><img src="{{ asset("uploads/author/".($post->user->profile ?? "default.webp")) }}" alt="{{ $post->user->name }}"/></li>
-                            <li class="post-author"> <a href="{{ route("frontend.user", $post->user->username) }}">{{ $post->user->name }}</a></li>
-                            <li class="entry-cat"> <a href="{{ route("frontend.category", $post->category->slug) }}" class="category-style-1 "><span class="line"></span>{{ $post->category->title }}</a></li>
-                            <li class="post-date"> <span class="line"></span>{{ $post->created_at->format("F d, Y") }}</li>
+                            <li class="post-author"><a href="{{ route("frontend.user", $post->user->username) }}">{{ $post->user->name }}</a></li>
+                            <li class="entry-cat"><a href="{{ route("frontend.category", $post->category->slug) }}" class="category-style-1"><span class="line"></span>{{ $post->category->title }}</a></li>
+                            <li class="post-date"><span class="line"></span>{{ $post->created_at->format("F d, Y") }}</li>
                         </ul>
                     </div>
                     <div class="post-single-content">
@@ -40,19 +40,16 @@
                             <p>Share on :</p>
                             <ul class="list-inline">
                                 <li>
-                                    <a href="{{ request()->url() }}"><i class="fab fa-facebook"></i></a>
+                                    <a href="{{ request()->url() }}" onclick="shareTo('facebook')"><i class="fab fa-facebook"></i></a>
                                 </li>
                                 <li>
-                                    <a href="{{ request()->url() }}"><i class="fab fa-instagram"></i></a>
+                                    <a href="javascript:void(0)" data-toggle="modal" data-target="#instagramModal"><i class="fab fa-instagram"></i></a>
                                 </li>
                                 <li>
-                                    <a href="{{ request()->url() }}"><i class="fab fa-twitter"></i></a>
+                                    <a href="{{ request()->url() }}" onclick="shareTo('twitter')"><i class="fab fa-twitter"></i></a>
                                 </li>
                                 <li>
-                                    <a href="{{ request()->url() }}"><i class="fab fa-youtube"></i></a>
-                                </li>
-                                <li>
-                                    <a href="{{ request()->url() }}"><i class="fab fa-pinterest"></i></a>
+                                    <a href="javascript:void(0)" data-toggle="modal" data-target="#youtubeModal"><i class="fab fa-youtube"></i></a>
                                 </li>
                             </ul>
                         </div>
@@ -64,4 +61,61 @@
         </div>
     </div>
 </section>
+
+<div class="modal fade" id="instagramModal" tabindex="-1" role="dialog" aria-labelledby="instagramModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="instagramModalLabel">Instagram Sharing</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Sorry, Instagram sharing is not supported currently.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="youtubeModal" tabindex="-1" role="dialog" aria-labelledby="youtubeModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="youtubeModalLabel">YouTube Sharing</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Sorry, YouTube sharing is not supported currently.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+function shareTo(platform) {
+    const url = encodeURIComponent(window.location.href);
+    let shareUrl = '';
+
+    switch (platform) {
+        case 'facebook':
+            shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
+            break;
+        case 'twitter':
+            shareUrl = `https://twitter.com/intent/tweet?url=${url}`;
+            break;
+    }
+
+    window.open(shareUrl, '_blank');
+}
+</script>
 @endsection
+
