bff2aa6950019f4c268955daaca47a2a93b9994c
Isaac Talb <thaungahlaybaw@gmail.com>
2025-07-05 01:41:10 +0700

    update for the Contact Us with SEO & Favicon Errors

--- resources/views/frontend/contact-us.blade.php
+++ resources/views/frontend/contact-us.blade.php
@@ -5,7 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <link rel="icon" sizes="16x16" href="{{ asset("favicon.ico") }}"/>
-    <title>@yield("title")</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("apple-touch-icon.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("dc_logo_750_750.png") }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("favicon.ico") }}">
+    <link rel="manifest" href="{{ asset("site.webmanifest") }}">
+    <link rel="mask-icon" href="{{ asset("safari-pinned-tab.svg") }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="keywords" content="Artificial Intelligence, AI, Innovative, 2025, IT, Tech, Blogs, news, tutorials, software development, IT infrastruct>
+    <meta name="description" content="At Duck Cloud, we're passionate about making tech knowledge accessible for everyone—from solo developers and start>
+    <meta property="og:title" content="Duck Cloud - Learning Platform for IT, Tech, and Software Development">
+    <meta property="og:description" content="At Duck Cloud, we're passionate about making tech knowledge accessible for everyone—from solo developers an>
+    <meta property="og:image" content="{{ asset("dc_logo_750_750.png") }}">
+    <title>Contact | Duck Cloud</title>
     <link rel="stylesheet" href="{{ asset("assets/frontend/css/bootstrap.min.css") }}"/>
     <link rel="stylesheet" href="{{ asset("assets/frontend/css/owl.carousel.css") }}"/>
     <link rel="stylesheet" href="{{ asset("assets/frontend/css/line-awesome.min.css") }}"/>
